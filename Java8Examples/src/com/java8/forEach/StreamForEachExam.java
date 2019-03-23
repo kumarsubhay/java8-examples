545c3e74ae6a7406e767224fa16fb28dacb6b753
subhay <subhaykumar88@gmail.com>
2019-03-23 19:52:18 +0530

    Java 8 Stream ForEach example

---
+++ Java8Examples/src/com/java8/forEach/StreamForEachExam.java
@@ -0,0 +1,19 @@
+package com.java8.forEach;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class StreamForEachExam {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> numberList = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+		
+		Consumer<Integer> action = System.out::println;
+		 
+		numberList.stream().filter(n -> n%2  == 0).forEach( action );
+
+	}
+
+}
